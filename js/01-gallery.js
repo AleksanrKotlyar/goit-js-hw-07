67b7e3ff7f84a2a9b77a743b79ff1c1bb93808ef
AleksanrKotlyar <kotlyar.alek@gmail.com>
2022-08-24 19:10:05 +0300

    task 1

---
+++ js/01-gallery.js
@@ -0,0 +1,59 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+const galleryRef = document.querySelector(".gallery");
+const GalleryMarkup = createGalleryMarkup(galleryItems);
+
+function createGalleryMarkup(items) {
+	return items
+		.map(({ preview, original, description }) => {
+			return ` 
+     <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+  `;
+		})
+		.join("");
+}
+
+galleryRef.insertAdjacentHTML("beforeend", GalleryMarkup);
+
+function onClickImgCreateModal(e) {
+	e.preventDefault();
+	if (!e.target.classList.contains("gallery__image")) return;
+	const urlImg = e.target.getAttribute("data-source");
+
+	const instance = basicLightbox.create(`
+    <div class="modal">
+        <img
+      class="gallery__image"
+      src="${urlImg}"
+      data-source="${e.target.getAttribute("data-source")}"
+      alt="${e.target.getAttribute("alt")}"
+    />
+    </div>
+`);
+
+	instance.show();
+
+	if (instance.show()) {
+		window.addEventListener("keydown", onEscPress);
+	}
+
+	function onEscPress(evt) {
+		if (evt.code === "Escape") {
+			instance.close();
+			window.removeEventListener("keydown", onEscPress);
+		}
+	}
+}
+
+galleryRef.addEventListener("click", onClickImgCreateModal);
+
+console.log(galleryItems);
