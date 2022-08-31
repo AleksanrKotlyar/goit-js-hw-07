0f150103637e0aae65084bfcb02be6a001e7f982
AleksanrKotlyar <kotlyar.alek@gmail.com>
2022-08-31 13:32:01 +0300

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -27,36 +27,60 @@ galleryRef.insertAdjacentHTML("beforeend", GalleryMarkup);
 function onClickImgCreateModal(e) {
 	e.preventDefault();
 	if (!e.target.classList.contains("gallery__image")) return;
-	const urlImg = e.target.getAttribute("data-source");
+	const currentImageUrl = e.target.dataset.source;
 
-	const instance = basicLightbox.create(`
-    <div class="modal">
-        <img
-      class="gallery__image"
-      src="${urlImg}"
-      data-source="${e.target.getAttribute("data-source")}"
-      alt="${e.target.getAttribute("alt")}"
-    />
-    </div>
-`);
+	const instance = basicLightbox.create(
+		`
+		<img class="modal__image" src="${currentImageUrl}" />`,
+		{
+			onShow: () => {
+				window.addEventListener("keydown", onKeyPress);
+			},
+			onClose: () => {
+				window.removeEventListener("keydown", onKeyPress);
+			},
+		}
+	);
 
-	instance.show(() => {
-		window.addEventListener("keydown", onEscPress);
-		window.addEventListener("click", onEscPress);
-	});
+	instance.show();
 
-	function onEscPress(evt) {
-		if (evt.code === "Escape") {
-			instance.close(() => {
-				window.removeEventListener("keydown", onEscPress);
-			});
-		} else {
-			instance.close(() => {
-				window.removeEventListener("keydown", onEscPress);
-				window.removeEventListener("click", onEscPress);
-			});
+	function onKeyPress(event) {
+		const isKeyCode = event.code === "Escape";
+		if (isKeyCode) {
+			instance.close();
 		}
 	}
+
+	// 	const instance = basicLightbox.create(`
+	//     <div class="modal">
+	//         <img
+	//       class="gallery__image"
+	//       src="${urlImg}"
+	//       data-source="${e.target.getAttribute("data-source")}"
+	//       alt="${e.target.getAttribute("alt")}"
+	//     />
+	//     </div>
+	// `);
+
+	// 	instance.show(() => {
+	// 		window.addEventListener("keydown", onEscPress);
+	// 		window.addEventListener("click", onEscPress);
+	// 	});
+
+	// 	function onEscPress(evt) {
+	// 		if (evt.code === "Escape") {
+	// 			console.log("Escape");
+	// 			instance.close(() => {
+	// 				window.removeEventListener("keydown", onEscPress);
+	// 				window.addEventListener("click", onEscPress);
+	// 			});
+	// 		} else {
+	// 			instance.close(() => {
+	// 				window.removeEventListener("keydown", onEscPress);
+	// 				window.removeEventListener("click", onEscPress);
+	// 			});
+	// 		}
+	// 	}
 }
 
 galleryRef.addEventListener("click", onClickImgCreateModal);
